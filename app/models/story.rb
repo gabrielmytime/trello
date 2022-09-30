6d6c93c906fac1755611e41243368c2c399faf17
Dan Lucian-Gabriel <gabriel@mytime.com>
2022-09-30 17:42:45 +0300

    stuff added

--- app/models/story.rb
+++ app/models/story.rb
@@ -9,7 +9,7 @@ class Story < ApplicationRecord
   acts_as_paranoid
 
   scope :by_position, -> { order(position: :asc) }
-  scope :filter_by_status, ->(status) { where(status: status) }
+  scope :filter_by_status, -> (status) { where(status: status) }
   scope :filter_by_month, -> { where('due_date < ?', DateTime.now.next_month) }
   scope :filter_by_week, -> { where('due_date < ?', DateTime.parse('saturday') + 7) }
 end
