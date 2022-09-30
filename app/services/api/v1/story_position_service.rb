b3f6dccdbaff5ed33f8b120f5b8091a2e3b03de4
Dan Lucian-Gabriel <gabriel@mytime.com>
2022-09-30 19:30:50 +0300

    added ordering with update_all

--- app/services/api/v1/story_position_service.rb
+++ app/services/api/v1/story_position_service.rb
@@ -29,13 +29,37 @@ module Api
 
       # if our position > story.position we move down
       # otherwise we move up
-      def update_position(story:, position:, column:)
-        if story.position > position
-          move_down(position: story.position, column: column)
-        else
-          move_up(position: story.position, column: column)
-        end
-        story.update({ position: position })
+      def update_position_in_column(story:, position:, column:)
+        column.stories.update_all("position=case 
+          when position = #{current_position} then #{to_position} 
+          when position >= #{to_position} and #{to_position} < #{current_position} then position + 1 
+          when position <= #{to_position} and #{to_position} > #{current_position} then position - 1  
+        else position end"
+        )
+          
+      def change_column_and_position(story:, to_position:, to_column:, column:)
+        Column.where(column_id: [column.id,to_column]).update_all("
+          position = case 
+            when position >= #{to_position} and column_id = #{to_column} then position + 1
+            else position
+            end
+          ,column_id = case 
+            when position = #{current_position} and column_id = #{current_column} then #{to_column} 
+            else column_id 
+            end
+          ,position = case 
+            when position > #{current_position} and column_id = #{current_column} then position - 1
+            else position
+            end
+          ")
+      end
+
+        # if story.position > position
+        #   move_down(position: story.position, column: column)
+        # else
+        #   move_up(position: story.position, column: column)
+        # end
+        # story.update({ position: position })
       end
     end
   end
